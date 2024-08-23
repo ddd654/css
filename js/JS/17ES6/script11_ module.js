54bfd13b096750bd76902de018e49362520900a4
ddd654 <bound654@naver.com>
2024-08-23 14:08:04 +0900

    자바스크립트 ES6 문법

---
+++ js/JS/17ES6/script11_ module.js
@@ -0,0 +1,23 @@
+//모듈은 클래스파일
+// 변수, 함수, 등등 포함
+//
+//모듈을 외부로 보낼때는 
+// 여러값 = name export
+// 하나씩 = default export
+export const name = "사과";
+export const age = 20;
+export let info = () => {
+    console.log(name, age)
+}
+
+
+
+
+
+
+
+
+
+
+
+
